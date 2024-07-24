c789783ac96af0fcb7afe29999bcb9d917018e25
jarenmontano <jarenmontano@mail.weber.edu>
2024-07-24 12:15:08 -0400

    This has the added gemini ai functionality

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 # https://requests.readthedocs.io/en/latest/
 import json
 
+import google.generativeai as genai
+import os
+os.environ["API_KEY"] = "AIzaSyAWapJjEQeBq4X86Yexu8xfaHwD1uXElpA"
+
 app = Flask(__name__)
+
+# configuring the AI
+genai.configure(api_key=os.environ["API_KEY"])
+model = genai.GenerativeModel('gemini-1.5-flash')
+chat = model.start_chat(history=[])
+# for the markdown of gemini text
+from IPython.display import display
+from IPython.display import Markdown
+
+
+# global variables
+htmlstring = ""
+
 #           Basic structure of the return json
 # {
 #       ""id"": 53,
@@ -70,12 +87,12 @@ def get_from_all(jsonstring):
     htmlstring += '''</div>'''
     return htmlstring
 
-def get_players():
+def get_players_from_server():
     try:
         # this url is to my local host in .net
         url = "http://localhost:5029/mainresourses"
         response = requests.get(url)
-        print(response.text)
+        # print(response.text)
         jsonstring = json.loads(response.text)
         #return get_from_one(jsonstring)
         soccer_teams = get_from_all(jsonstring)
@@ -89,13 +106,41 @@ def get_players():
         # name = "FAILURE"
         # return name
 
+def get_players():
+    if(len(htmlstring) < 40):
+       soccer_team=  get_players_from_server()
+       return soccer_team
+    return htmlstring
+
+
+
+def use_ai(prompt):
+    prompt_response = ""
+    
+    prompt_response = chat.send_message(prompt)
+    # the one below is for one time, the one above is for chat(MULTIPLE with History)
+    # prompt_response = model.generate_content(prompt)
+    print(prompt_response.text)
+
+    return prompt_response.text
+
 
 @app.route("/")
 def index():
+    prompt = "Give me information on the Soccer Teams St. Johnstone"
     soccer_teams= get_players()
+    #prompt_response = use_ai(prompt)
     # name = get_players()
     return render_template("index.html", soccer_teams=soccer_teams)
     # return render_template("index.html",  name=name)
+
+@app.route("/aichat" , methods=["POST"])
+def return_ai_response():
+    user_prompt = request.form["user_prompt"]
+    print(user_prompt)
+    soccer_teams= get_players()
+    prompt_response = " Top Binz Response\n" + use_ai(user_prompt)
+    return render_template("index.html", soccer_teams=soccer_teams, previous_chat = chat.history, prompt_response = prompt_response)
 
 if __name__ == "__main__":
     app.run(debug=True)
